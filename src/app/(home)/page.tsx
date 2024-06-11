605c36320c60919a93470af8cbb779c2f0b65300
FragaGabriel23 <gabrielfraga2308@gmail.com>
2024-06-11 12:18:40 -0300

    Initial Setings Next.JS and SASS

---
+++ src/app/(home)/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import Style from './page.module.scss';
+
+const Home = () => {
+  return (
+    <>
+    </>
+  )
+}
+
+export default Home
---
+++ src/fonts/_fonts.tsx
@@ -0,0 +1,9 @@
+import { Inter, Open_Sans } from "next/font/google";
+
+export const inter = Inter({
+    subsets: ["latin"],
+});
+
+export const open_Sans = Open_Sans({
+    subsets: ["latin"],
+})
