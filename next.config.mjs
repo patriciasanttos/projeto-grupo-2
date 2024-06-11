08f7cce11f12dfaaff1a74ddaa8bc30a69dc08be
FragaGabriel23 <gabrielfraga2308@gmail.com>
2024-06-11 10:36:31 -0300

    build: Add SASS

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,10 @@
+const path = require('path');
+
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    sassOptions: {
+        includePaths: [path.join(process.cwd(), 'src', 'sass')],
+      },
+};
 
 export default nextConfig;
