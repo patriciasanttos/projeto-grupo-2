cf2d7a727426685a7f09aace8abb9211bfc23826
FragaGabriel23 <gabrielfraga2308@gmail.com>
2024-06-18 01:46:05 -0300

    feat: Added header

--- front/src/fonts/_fonts.tsx
+++ front/src/fonts/_fonts.tsx
@@ -1,14 +1,15 @@
-import { Inter, Open_Sans, Roboto } from "next/font/google";
+import { Inter, Open_Sans, Roboto } from 'next/font/google';
 
 export const inter = Inter({
-    subsets: ["latin"],
+  weight: ['100', '300', '400', '500', '700', '900'],
+  subsets: ['latin'],
 });
 
 export const open_Sans = Open_Sans({
-    subsets: ["latin"],
-})
+  subsets: ['latin'],
+});
 
 export const roboto = Roboto({
-    weight: ["100", "300", "400", "500", "700", "900"],
-    subsets: ["latin"],
-})
+  weight: ['100', '300', '400', '500', '700', '900'],
+  subsets: ['latin'],
+});
