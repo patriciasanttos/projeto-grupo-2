248fe83f7b99a2af02da784d1812afe05f82826b
Diogo Rodrigues <diogogamerpudim@gmail.com>
2024-06-21 21:39:00 -0300

    fix: adjusting landing page

--- front/src/app/(LandingPage)/page.tsx
+++ front/src/app/(LandingPage)/page.tsx
@@ -1,9 +1,8 @@
 import React from 'react';
 import Style from './page.module.scss';
-import { FlipList } from '../../components/FlipCards/FlipList/FlipList';
 
 const LandingPage = () => {
-  return <FlipList />;
+  return <></>;
 };
 
 export default LandingPage;
