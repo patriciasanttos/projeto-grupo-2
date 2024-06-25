0d19778f9bbda4495f6285fa1a839d0ddb534d17
FragaGabriel23 <gabrielfraga2308@gmail.com>
2024-06-25 01:56:16 -0300

    feat(app/calculator): Added logo in header

--- front/src/app/calculator/page.tsx
+++ front/src/app/calculator/page.tsx
@@ -1,8 +1,19 @@
+'use client';
 import React from 'react';
 import Style from './page.module.scss';
+import { Box, ThemeProvider } from '@mui/material';
+import { LightTheme } from '@/themes';
+import Image from 'next/image';
+import Logo from '../../../public/logo.svg';
 
 const Calculator = () => {
-  return <></>;
+  return (
+    <ThemeProvider theme={LightTheme}>
+      <Box component="header" className={Style.header}>
+        <Image src={Logo} alt="Logo Equipacare" className={Style.header__img} />
+      </Box>
+    </ThemeProvider>
+  );
 };
 
 export default Calculator;
