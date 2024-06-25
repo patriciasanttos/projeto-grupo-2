3446b03de5b72aaef2c2b3fce4087859af87ddac
FragaGabriel23 <gabrielfraga2308@gmail.com>
2024-06-25 02:24:15 -0300

    feat(app/calculator): Created card and aside

--- front/src/app/calculator/page.tsx
+++ front/src/app/calculator/page.tsx
@@ -1,7 +1,7 @@
 'use client';
 import React from 'react';
 import Style from './page.module.scss';
-import { Box, ThemeProvider } from '@mui/material';
+import { Box, ThemeProvider} from '@mui/material';
 import { LightTheme } from '@/themes';
 import Image from 'next/image';
 import Logo from '../../../public/logo.svg';
@@ -11,6 +11,10 @@ const Calculator = () => {
     <ThemeProvider theme={LightTheme}>
       <Box component="header" className={Style.header}>
         <Image src={Logo} alt="Logo Equipacare" className={Style.header__img} />
+      </Box>
+      <Box component="section" className={Style.calculator}>
+        <Box component="aside" className={Style.calculator__aside}></Box>
+        <Box component="main" className={Style.calculator__card}></Box>
       </Box>
     </ThemeProvider>
   );
