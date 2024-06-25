f5c8abf1a9719b74c26548f14c5ac3adc3a0768a
FragaGabriel23 <gabrielfraga2308@gmail.com>
2024-06-24 23:44:30 -0300

    refactor: Correcting service card images

--- front/src/components/ServiceCards/ServiceCard/ServiceCardList.tsx
+++ front/src/components/ServiceCards/ServiceCard/ServiceCardList.tsx
@@ -8,19 +8,19 @@ const ServiceCardList = () => {
       <FlipCard
         text="A reputação de um hospital é fortemente influenciada pela segurança e qualidade dos seus serviços. A transparência sobre os processos do CME e a garantia de sua eficácia aumentam a confiança dos pacientes e da comunidade médica na instituição, fortalecendo sua reputação."
         frontText="Segurança do paciente"
-        image="quality.svg"
+        image="patient.svg"
         alt="dollar"
       />
       <FlipCard
         text="A reputação de um hospital é fortemente influenciada pela segurança e qualidade dos seus serviços. A transparência sobre os processos do CME e a garantia de sua eficácia aumentam a confiança dos pacientes e da comunidade médica na instituição, fortalecendo sua reputação."
         frontText="Qualidade dos Serviços de Saúde"
-        image="hourglass.svg"
+        image="healthcare.svg"
         alt="hourglass"
       />
       <FlipCard
         text="A esterilização correta dos instrumentos cirúrgicos e médicos é fundamental para evitar infecções hospitalares, que podem levar a complicações graves ou até à morte do paciente. Conhecer e avaliar o CME do hospital garante que todos os processos estejam alinhados com os padrões de segurança e higiene."
         frontText="Confiança e Reputação"
-        image="dollar.svg"
+        image="reputation.svg"
         alt="dollar"
       />
     </main>
