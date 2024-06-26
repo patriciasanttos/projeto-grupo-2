274a56bed64fc27e1a3c04c0d30c173752b2983a
FragaGabriel23 <gabrielfraga2308@gmail.com>
2024-06-26 06:03:07 -0300

    feat(app/result): Added VeticalTab

--- front/src/app/calculator/result/page.tsx
+++ front/src/app/calculator/result/page.tsx
@@ -1,8 +1,27 @@
+'use client'
 import React from 'react';
 import Style from './page.module.scss';
+import { Box, ThemeProvider } from '@mui/material';
+import Image from 'next/image';
+import { LightTheme } from '@/themes';
+import Logo from '../../../../public/logo.svg';
+import VerticalTab from '@/components/VerticalTab/VerticalTab';
 
 const Result = () => {
-  return <></>;
+  return (
+    <>
+      <ThemeProvider theme={LightTheme}>
+        <Box component="header" className={Style.header}>
+          <Image
+            src={Logo}
+            alt="Logo Equipacare"
+            className={Style.header__img}
+          />
+        </Box>
+        <VerticalTab brands={''/*todo*/}/>
+      </ThemeProvider>
+    </>
+  );
 };
 
 export default Result;
