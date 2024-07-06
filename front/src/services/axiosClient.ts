23ec9b1aebaab17b181b5a6ee2921ac8a03bf32c
FragaGabriel23 <gabrielfraga2308@gmail.com>
2024-07-06 03:00:04 -0300

    feat: Created Axios instance

---
+++ front/src/services/axiosClient.ts
@@ -0,0 +1,10 @@
+import axios from 'axios';
+
+const axiosClient = axios.create({
+  baseURL: process.env.NEXT_PUBLIC_API_BASE_URL,
+  headers: {
+    'Content-Type': 'application/json',
+  },
+});
+
+export default axiosClient;
