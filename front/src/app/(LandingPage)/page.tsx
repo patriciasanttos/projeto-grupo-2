ae78f6ba6d74d4d662445f3a391a2866bf1705f0
FragaGabriel23 <gabrielfraga2308@gmail.com>
2024-07-07 02:36:49 -0300

    feat(app/LandingPage): Added query client provider

--- front/src/app/(LandingPage)/page.tsx
+++ front/src/app/(LandingPage)/page.tsx
@@ -12,6 +12,7 @@ import AboutToolList from '@/components/AboutToolList/AboutToolList';
 import ServiceCardList from '@/components/ServiceCard/ServiceCardList';
 import Footer from '@/components/Footer/Footer';
 import { ActionLandingPage, StateLandinPage } from '@/types';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 
 const Reducer = (state: StateLandinPage, action: ActionLandingPage) => {
   switch (action.type) {
@@ -20,7 +21,7 @@ const Reducer = (state: StateLandinPage, action: ActionLandingPage) => {
     case 'SET_CARD':
       return { ...state, card: action.payload };
     case 'SET_ERROR':
-      return { ...state, errors: {...state.errors, ...action.payload } };
+      return { ...state, errors: { ...state.errors, ...action.payload } };
     default:
       return state;
   }
@@ -58,66 +59,70 @@ const LandingPage = () => {
     }
   };
 
+  const queryClient = new QueryClient();
+
   return (
-    <ThemeProvider theme={LightTheme}>
-      <NavBar />
-      <Box component="header" className={Style.header}>
-        <Image
-          src={HeaderImage}
-          alt="CME"
-          priority
-          className={Style.header__img}
-          fill
-        />
-        <Box className={Style.header__title}>
-          <Typography variant="h2" fontWeight="900" color="white">
-            Não espere mais para transformar a gestão do seu hospital!
-          </Typography>
-        </Box>
-        <Box display="flex" justifyContent="center">
-          <Box className={Style.header__subTitle}>
-            <Typography variant="h5" fontWeight="600">
-              Otimize a gestão de recursos com nossa ferramenta de cálculo de
-              CME
+    <QueryClientProvider client={queryClient}>
+      <ThemeProvider theme={LightTheme}>
+        <NavBar />
+        <Box component="header" className={Style.header}>
+          <Image
+            src={HeaderImage}
+            alt="CME"
+            priority
+            className={Style.header__img}
+            fill
+          />
+          <Box className={Style.header__title}>
+            <Typography variant="h2" fontWeight="900" color="white">
+              Não espere mais para transformar a gestão do seu hospital!
             </Typography>
           </Box>
+          <Box display="flex" justifyContent="center">
+            <Box className={Style.header__subTitle}>
+              <Typography variant="h5" fontWeight="600">
+                Otimize a gestão de recursos com nossa ferramenta de cálculo de
+                CME
+              </Typography>
+            </Box>
+          </Box>
         </Box>
-      </Box>
-      <Box component="section" className={Style.cardForm}>
-        <Container maxWidth="lg" className={Style.cardForm__card}>
-          {renderCardForm(state.card)}
-        </Container>
-      </Box>
-      {state.card != 'initial' ? (
-        <Container maxWidth="lg" className={Style.aboutTool}>
+        <Box component="section" className={Style.cardForm}>
+          <Container maxWidth="lg" className={Style.cardForm__card}>
+            {renderCardForm(state.card)}
+          </Container>
+        </Box>
+        {state.card != 'initial' ? (
+          <Container maxWidth="lg" className={Style.aboutTool}>
+            <Typography
+              variant="h6"
+              fontWeight="600"
+              textAlign="center"
+              color={'white'}
+            >
+              Descubra a revolução na Gestão de Materiais Hospitalares com Nossa
+              Ferramenta de Cálculo de CME
+            </Typography>
+            <AboutToolList />
+          </Container>
+        ) : (
+          ''
+        )}
+        <Container className={Style.serviceCards}>
           <Typography
-            variant="h6"
+            variant="h5"
             fontWeight="600"
             textAlign="center"
             color={'white'}
+            marginBottom={'2vh'}
           >
-            Descubra a revolução na Gestão de Materiais Hospitalares com Nossa
-            Ferramenta de Cálculo de CME
+            A importância de garantir equipamentos adequados
           </Typography>
-          <AboutToolList />
+          <ServiceCardList />
         </Container>
-      ) : (
-        ''
-      )}
-      <Container className={Style.serviceCards}>
-        <Typography
-          variant="h5"
-          fontWeight="600"
-          textAlign="center"
-          color={'white'}
-          marginBottom={'2vh'}
-        >
-          A importância de garantir equipamentos adequados
-        </Typography>
-        <ServiceCardList />
-      </Container>
-      <Footer />
-    </ThemeProvider>
+        <Footer />
+      </ThemeProvider>
+    </QueryClientProvider>
   );
 };
 
