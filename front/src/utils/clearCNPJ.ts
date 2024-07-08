e191bf6e14ef9ec7d1bc7b780daa8b48922c6e38
FragaGabriel23 <gabrielfraga2308@gmail.com>
2024-07-08 11:25:10 -0300

    fix: fix get method URL

--- front/src/utils/clearCNPJ.ts
+++ front/src/utils/clearCNPJ.ts
@@ -1,5 +1,5 @@
 export const clearCNPJ = (cnpj: string) => {
   const cleanCNPJ = cnpj.replace(/[./-]/g, '');
-  console.log(cleanCNPJ)
-  return { cnpj: cleanCNPJ };
+  console.log(cleanCNPJ);
+  return cleanCNPJ;
 };
