52af98ea945f653156637acacdddd228689c0b44
Patricia Santos <patricia.desenvolveti@gmail.com>
2024-07-17 21:46:56 -0300

    Added icons in Layout

--- front/src/app/(LandingPage)/layout.tsx
+++ front/src/app/(LandingPage)/layout.tsx
@@ -1,9 +1,29 @@
-import type { Metadata } from "next";
-import { inter } from "../../fonts/_fonts";
-import "../globals.scss";
+import React from 'react';
+import type { Metadata } from 'next';
+import { inter } from '../../fonts/_fonts';
+import '../globals.scss';
 
 export const metadata: Metadata = {
-  title: "Equipacare",
+  title: 'Equipacare',
+  icons: {
+    icon: [
+      {
+        rel: 'icon',
+        url: '../public/logo32x32.png',
+        sizes: '32x32',
+      },
+      {
+        rel: 'icon',
+        url: '../public/logo192x192.png',
+        sizes: '192x192',
+      },
+      {
+        rel: 'icon',
+        url: '../public/logo180x180.png',
+        sizes: '180x180',
+      },
+    ],
+  },
 };
 
 export default function RootLayout({
